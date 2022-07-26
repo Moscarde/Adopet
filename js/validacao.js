f8b9ea653c505dea1a2a1e3afbe5911e134a456d
Moscarde <theizera@gmail.com>
2022-07-26 05:51:34 -0300

    validacao de formulario

---
+++ js/validacao.js
@@ -0,0 +1,62 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo
+
+    if (validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
+
+    if (input.validity.valid) {
+        input.classList.remove('cadastro-form__input-invalido')
+        input.nextElementSibling.innerHTML = '&#10240'
+    } else {
+        input.classList.add('cadastro-form__input-invalido')
+        input.nextElementSibling.innerHTML = mostraMensagemDeErro(tipoDeInput, input)
+    }
+}
+
+const validadores = {
+    senhaRepetida: input => validaSenhaRepetida(input)
+}
+
+
+const tiposDeErro = ['valueMissing', 'typeMismatch', 'patternMismatch', 'customError']
+
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo nome não pode estar vazio.'
+    },
+    email: {
+        valueMissing: 'O campo email não pode estar vazio.',
+        typeMismatch: 'O email digitado não é valido.'
+    },
+    senha: {
+        valueMissing: 'O campo senha não pode estar vazio.',
+        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.'
+    },
+    senhaRepetida: {
+        valueMissing: 'O campo confirma senha não pode estar vazio',
+        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.',
+        customError: 'Senha inserida não corresponde a senha digitada anteriormente.'
+    }
+}
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = ''
+    tiposDeErro.forEach(erro => {
+        if (input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro]
+        }
+    })
+    return mensagem
+}
+
+function validaSenhaRepetida(input) {
+    const senha = document.getElementById("cadastro-form__senha").value
+
+    let mensagem = ""
+    if (input.value !== senha) {
+        mensagem = "xxxxx"
+    }
+
+    input.setCustomValidity(mensagem)
+}
