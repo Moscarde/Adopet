ecf58bcef30fd247a08efa8ff277f4734a411c03
Moscarde <theizera@gmail.com>
2022-07-31 23:09:29 -0300

    api web

--- js/host.mjs
+++ js/host.mjs
@@ -1,2 +1,3 @@
-export let host = "http://localhost:8877"
+export let host = "https://api-adopet-moscarde.herokuapp.com"
+// export let host = "http://localhost:8877"
 // export let host = "http://localhost:3000"
